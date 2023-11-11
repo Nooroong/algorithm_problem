9d03960c6cf385ff0ec7c59231f9b043559e619a
Nooroong <46676443+Nooroong@users.noreply.github.com>
2023-11-11 15:45:28 +0900

    [D3] Title: 현주의 상자 바꾸기, Time: 454 ms, Memory: 62,036 KB -BaekjoonHub

---
+++ SWEA/D3/5789. 현주의 상자 바꾸기/현주의 상자 바꾸기.py
@@ -0,0 +1,13 @@
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    n, q = map(int, input().split())
+    box = [0]*n
+    
+    for i in range(1, q+1):
+        l, r = map(int, input().split()) #Li, Ri
+        for j in range(l-1, r):
+            box[j] = i
+            
+    print(f"#{test_case} ", end='')
+    print(*box)
