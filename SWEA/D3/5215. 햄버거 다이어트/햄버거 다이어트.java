4dcc2acd73fa6a3a29c9d79c881105d6ec3c63fd
Nooroong <46676443+Nooroong@users.noreply.github.com>
2024-02-14 14:09:28 +0900

    [D3] Title: 햄버거 다이어트, Time: 723 ms, Memory: 20,964 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,14 +1,35 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /**
+ * SWEA
+ * @author eunwoo.lee
  * 
- * @author JiYeon Sin
- * 1. 테스트 케이스 갯수를 입력받는다.
- * 2. 재료의 수, 제한 칼로리를 입력받는다.
- * 3. 맛 점수와 칼로리를 입력받아 배열에 저장한다.
- * 4-1. 조합으로 푼다.
- * 4-2. 부분집합으로 푼다.
+ * <powerSet>
+ * 1. 기저 조건
+ * 	모든 원소를 선택했다면
+ *  	선택된 원소들의 칼로리 합, 점수 합 구하기
+ * 		선택된 원소들의 칼로리 합이 제한 칼로리를 넘지 않는다면
+ * 			점수의 총합이 다른 조합의 점수보다 크다면 갱신
+ * 	return
+ * 2. 해당 원소를 사용한다면
+ * 	2-2. 전처리 로직 : 해당 원소 사용 true
+ * 	2-3. 재귀 호출
+ * 3. 해당 원소를 사용하지 않는다면
+ * 	3-2. 전처리 로직 : 해당 원소 사용 false
+ * 	3-3. 재귀 호출
+ * 
+ * 
+ * <main>
+ * 1. 입력
+ * 	1-1. 테스트케이스 수를 입력받는다.
+ * 	1-2. 테스트케이스 수만큼 반복한다.
+ * 	1-3. 재료의 수, 제한 칼로리를 입력받는다.
+ * 	1-4. 재료의 수만큼 맛 점수와 칼로리를 입력받는다.
+ * 2. 부분집합 재귀 실행
+ * 3. 점수 최대값 출력
  *
  */
 
@@ -17,127 +38,88 @@ public class Solution {
 	public static StringBuilder sb;
 	public static StringTokenizer st;
 	
-	public static int numOfIngre; // 재료의 수
-	public static int calLimit; // 제한 칼로리
+	public static boolean[] elementUsedList;
+	public static int maxScore; // 맛 점수 최대값
 	
-	public static int[] taste; // 맛 점수를 저장하는 배열
-	public static int [] cal; // 칼로리를 저장하는 배열
+	public static int testCase; // 테스트케이스의 수
+	public static int ingredNum, limitCal; // 재료의 수, 제한 칼로리
+	public static int[] score; // 맛 점수 저장 배열
+	public static int[] calorie; // 칼로리 저장 배열
 	
-	public static int maxTaste; // 최고 맛점수
-	
-	public static int[][] selectElementList; // 내가 선택한 원소들을 담아놓는 배열. 맛과 칼로리 순서로 담는다.
-	public static boolean[] elementUsedCheckList; // 원소의 사용여부를 체크하는 배열
-	
-	
-	public static void main(String[] args) throws IOException {
+	public static void powerSet(int selectIdx) {
+		
+		// 1. 기저 조건
+		// 모든 원소를 선택했다면
+		if (selectIdx==ingredNum) {
+			int selectedCalSum=0;
+			int selectedScoreSum=0;
+			// 선택된 원소들의 칼로리 합, 점수 합 구하기
+			for (int usedIdx=0; usedIdx<ingredNum; usedIdx++) {
+				if (elementUsedList[usedIdx]==true) {
+					selectedCalSum += calorie[usedIdx];
+					selectedScoreSum += score[usedIdx];
+				}
+			}
+			// 선택된 원소들의 칼로리 합이 제한 칼로리를 넘지 않는다면
+			if (selectedCalSum<=limitCal) {
+				if (selectedScoreSum>maxScore) {
+					maxScore = selectedScoreSum;
+				}
+			}
+			return;
+		}
+		
+		// 2. 해당 원소를 사용한다면 
+		// 2-2. 전처리 로직 : 해당 원소 사용 true
+		elementUsedList[selectIdx] = true;
+		// 2-3. 재귀 호출
+		powerSet(selectIdx+1);
+		
+		// 3. 해당 원소를 사용한다면 
+		// 3-2. 전처리 로직 : 해당 원소 사용 true
+		elementUsedList[selectIdx] = false;
+		// 3-3. 재귀 호출
+		powerSet(selectIdx+1);
+		
+		
+	}
+
+	public static void main(String[] args) throws Exception{
 		br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
 		
-		
-		// 1. 테스트 케이스 갯수를 입력받는다.
-		int testCase = Integer.parseInt(br.readLine().trim());
-		
-		
-		
-		for(int tc = 1; tc <= testCase; tc++) {
-			// 2. 재료의 수, 제한 칼로리를 입력받는다.
+		// 1. 입력
+		// 1-1. 테스트케이스 수를 입력받는다.
+		st = new StringTokenizer(br.readLine().trim());
+		testCase = Integer.parseInt(st.nextToken());
+			
+		// 1-2. 테스트케이스 수만큼 반복한다.
+		for (int tc=1; tc<=testCase; tc++) {
+			sb.append("#"+tc+" ");
+			
+			// 1-3. 재료의 수, 제한 칼로리를 입력받는다.
 			st = new StringTokenizer(br.readLine().trim());
-			numOfIngre = Integer.parseInt(st.nextToken());
-			calLimit = Integer.parseInt(st.nextToken());
+			ingredNum = Integer.parseInt(st.nextToken());
+			limitCal = Integer.parseInt(st.nextToken());
 			
-			// 3. 맛 점수와 칼로리를 입력받아 배열에 저장한다.
-			taste = new int[numOfIngre];
-			cal = new int[numOfIngre];
-			for(int idx = 0; idx < numOfIngre; idx++) {
+			// 1-4. 재료의 수만큼 맛 점수와 칼로리를 입력받는다.
+			score = new int[ingredNum];
+			calorie = new int[ingredNum];
+			for (int ingredIdx=0; ingredIdx<ingredNum; ingredIdx++) {
 				st = new StringTokenizer(br.readLine().trim());
-				taste[idx] = Integer.parseInt(st.nextToken());
-				cal[idx] = Integer.parseInt(st.nextToken());
-			}
+				score[ingredIdx] = Integer.parseInt(st.nextToken());
+				calorie[ingredIdx] = Integer.parseInt(st.nextToken());
+			}		
 			
-			// 4-1. 조합으로 푼다.
-			selectElementList = new int[numOfIngre][2];
-			maxTaste = 0;
-			combination(0, 0);
-			sb.append("#").append(tc).append(" ").append(maxTaste).append("\n");
-
-			
-//			// 4-2. 부분집합으로 푼다.
-//			elementUsedCheckList = new boolean[numOfIngre];
-//			maxTaste = 0;
-//			powerSet(0);
-//			sb.append("#").append(tc).append(" ").append(maxTaste).append("\n");
+			// 2. 부분집합 재귀 실행
+			elementUsedList = new boolean[ingredNum];
+			maxScore = 0;
+			powerSet(0);
+			// 3. 점수 최대값 출력
+			sb.append(maxScore+"\n");
 		}
 		System.out.println(sb);
-	}
-	
-	
-	public static void combination(int selectIdx, int elementIdx) {
-		// 1-1. 종료조건: 칼로리가 초과된 경우
-		int totalCal = 0;
-		for(int idx = 0; idx < selectIdx; idx++) {
-			totalCal += selectElementList[idx][1];
-		}
-		if(totalCal > calLimit) {
-			return;
-		}
-		
-		
-		// 2. 전처리: 칼로리가 초과되지 않았으면 매번 최대 맛점수를 갱신한다.
-		int totalTaste = 0;
-		for(int idx = 0; idx < selectIdx; idx++) {
-			totalTaste += selectElementList[idx][0];
-		}
-		maxTaste = (totalTaste > maxTaste) ? totalTaste : maxTaste;
-		
-		
-		// 1-2. 종료조건: 모든 음식들을 다 살펴본 경우
-		if(elementIdx == numOfIngre) {
-			return;
-		}
-		
-		
 
-		// 3. 재귀호출: 현재 음식을 고르는 경우
-		selectElementList[selectIdx][0] = taste[elementIdx];
-		selectElementList[selectIdx][1] = cal[elementIdx];
-		combination(selectIdx + 1, elementIdx + 1);
-		
-		// 3. 재귀호출: 현재 음식을 안 고르는 경우
-		selectElementList[selectIdx][0] = 0;
-		selectElementList[selectIdx][1] = 0;
-		combination(selectIdx, elementIdx + 1);
 	}
-	
-	
-	
-	public static void powerSet(int selectIdx) {
-		// 1. 종료조건 : 모든 음식을 살펴본 경우
-		if(selectIdx == numOfIngre) {
-			int totalTaste = 0;
-			int totalCal = 0;
-			for(int idx = 0; idx < numOfIngre; idx++) {
-				if(elementUsedCheckList[idx]) {
-					totalTaste += taste[idx];
-					totalCal += cal[idx];
-				}
-			}
-			
-			if(totalCal <= calLimit) {
-				maxTaste = (totalTaste > maxTaste) ? totalTaste : maxTaste;
-			}
-			
-			return;
-		}
-		
-		
-		
-		// 3. 재귀호출: 현재 음식을 고르는 경우
-		elementUsedCheckList[selectIdx] = true;
-		powerSet(selectIdx+ 1);
-		
-		// 3. 재귀호출: 현재 음식을 안 고르는 경우
-		elementUsedCheckList[selectIdx] = false;
-		powerSet(selectIdx + 1);
-	}
+
 }
- 
