3c815f233c76926acae7c07dacca5d73f7a64504
Nooroong <46676443+Nooroong@users.noreply.github.com>
2024-02-14 16:32:23 +0900

    [Gold IV] Title: 알파벳, Time: 1004 ms, Memory: 12292 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,106 +1,102 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 /**
+ * BOJ
+ * @author eunwoo.lee
  * 
- * @author JiYeon Sin
- * 1. 방문한적 있는 알파벳에 대한 정보를 저장하는 배열을 선언
- * 2. 보드의 크기를 입력받는다.
- * 3. 보드의 알파벳 정보를 입력받는다.
- * 4. 0, 0부터 시작한다.
- * 5. 이동거리 1부터 시작하여 dfs로 탐색을 한다.
- * 6. 현재 노드에 대해 방문처리를 한다.
- * 7. 인접한 칸을 살핀다.
- * 8. 보드의 범위를 벗어나지 않으면서 이전에 방문하지 않은 노드라면 방문을 한다.
- * 9. 더이상 방문할 노드가 없다면 최대 이동 거리를 갱신하고 이전 노드로 돌아간다.
- * 10. 탐색이 끝나면 최대 이동 거리를 출력한다.
+ * <dfs> : 2차원 배열 dfs+backtracking
+ * 1. 기저조건
+ * 	현재 칸의 알파벳이 이미 사용되었다면
+ * 	현재까지의 누적이동칸과 maxBox 비교해 갱신
+ * 	return
+ * 2. 전처리 로직
+ * 	현재 칸 알파벳 방문처리
+ * 3. 재귀호출
+ * 	다음 인덱스 만들기
+ * 		다음 인덱스가 범위 안에 있다면, 재귀호출
+ * 4. 후처리 로직
+ * 	현재 알파벳 방문 상태 false로 되돌려 주기 
+ * 
+ * <main>
+ * 1. 입력
+ * 	1-1. 세로 칸 수, 가로 칸 수를 입력받는다.
+ * 	1-2. 보드의 알파벳을 입력받는다.
+ * 2. dfs(0,0,0)
+ * 3. 정답 출력
  */
 
 public class Main {
+	
 	public static BufferedReader br;
+	public static StringBuilder sb;
 	public static StringTokenizer st;
 	
-	// 상, 우, 하, 좌
-	public static int[] deltaRow = { -1, 0, 1, 0 };
-	public static int[] deltaCol = { 0, 1, 0, -1 };
-	public static final int DIRECTION = 4; // 이동할 수 있는 방향의 수
-	
-	public static char[][] board; // 말이 놓이는 보드
-	public static int rowSize; // 보드의 세로 사이즈
-	public static int colSize; // 보드의 가로 사이즈
-	
-	// 4. 0, 0부터 시작한다.
-	public static final int START_ROW = 0; // 말의 시작 행 위치
-	public static final int START_COL = 0; // 말의 시작 열 위치
-	
-	public static final int NUM_OF_CHAR = 26; // 보드에 사용되는 문자의 개수
-	public static boolean[] visited; // 방문한 알파벳에 대한 정보를 저장하는 배열
-	public static int maxDepth; // 최대 이동 거리
+	// 상하좌우 delta 배열
+	public static final int[] dr = {-1, 1, 0, 0};
+	public static final int[] dc = {0, 0, -1, 1};
+	public static boolean[] isVisited;
 	
 	
+	public static int row, col;
+	public static char[][] board;
+	public static int maxBox; // 이동한 최대 칸 저장
 	
-	
-	public static void main(String[] args) throws IOException {
+	public static void dfs(int currentRowIdx, int currentColIdx, int accumBoxNum) {
+		
+		// 1. 기저조건
+		if (isVisited[board[currentRowIdx][currentColIdx]-'A']) {
+			maxBox = Math.max(accumBoxNum, maxBox);
+			return;
+		}
+		// 2. 전처리 로직
+		// 현재 알파벳 방문 처리
+		isVisited[board[currentRowIdx][currentColIdx]-'A'] = true;
+		
+		// 3. 재귀호출
+		for (int d=0; d<4; d++) {
+			int nextRowIdx = currentRowIdx+dr[d];
+			int nextColIdx = currentColIdx+dc[d];
+			// 다음 인덱스가 범위 안에 있다면
+			if (nextRowIdx>=0 && nextRowIdx<row && nextColIdx>=0 && nextColIdx<col) {
+				dfs(nextRowIdx, nextColIdx, accumBoxNum+1);
+			}
+		}
+		
+		// 4. 후처리 로직
+		isVisited[board[currentRowIdx][currentColIdx]-'A'] = false;
+		
+		
+	}
+
+	public static void main(String[] args) throws Exception{
 		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
 		
-		
-		// 1. 방문한적 있는 알파벳에 대한 정보를 저장하는 배열을 선언
-		visited = new boolean[NUM_OF_CHAR];
-		
-		
-		// 2. 보드의 크기를 입력받는다.
+		// 1. 입력
+		// 1-1. 세로칸 수, 가로칸 수를 입력받는다.
 		st = new StringTokenizer(br.readLine().trim());
-		rowSize = Integer.parseInt(st.nextToken());
-		colSize = Integer.parseInt(st.nextToken());
-		
-		
-		// 3. 보드의 알파벳 정보를 입력받는다.
-		board = new char[rowSize][colSize];
-		for(int rowIdx = 0; rowIdx < rowSize; rowIdx++) {
+		row = Integer.parseInt(st.nextToken());
+		col = Integer.parseInt(st.nextToken());
+		// 1-2. 보드의 알파벳을 입력받는다.
+		board = new char[row][col];
+		for (int inputRowIdx=0; inputRowIdx<row; inputRowIdx++) {
 			String line = br.readLine().trim();
-			for(int colIdx = 0; colIdx < colSize; colIdx++) {
-				board[rowIdx][colIdx] = line.charAt(colIdx);
+			for (int inputColIdx=0; inputColIdx<col; inputColIdx++) {
+				board[inputRowIdx][inputColIdx] = line.charAt(inputColIdx);
 			}
 		}
 		
-	
-		
-		// 5. 이동거리 1부터 시작하여 dfs로 탐색을 한다.
-		maxDepth = 0;
-		dfs(0, 0, 1);
-		
-		
-		// 10. 탐색이 끝나면 최대 이동 거리를 출력한다.
-		System.out.println(maxDepth);
+		// 2. 배열 탐색 실행
+		isVisited = new boolean[26];
+		maxBox = 1;
+		dfs(0, 0, 0);
+		// 3. 정답 출력
+		System.out.println(maxBox);
+
 	}
-	
-	
-	
-	
-	public static void dfs(int curRow, int curCol, int depth) {
-		// 6. 현재 노드에 대해 방문처리를 한다.
-		visited[ board[curRow][curCol] - 'A' ] = true;
-		
-		int newRow, newCol; // 말이 새롭게 이동하게 될 좌표
-		
-		
-		// 7. 인접한 칸을 살핀다.
-		for(int dir = 0; dir < DIRECTION; dir++) {
-			newRow = curRow + deltaRow[dir];
-			newCol = curCol + deltaCol[dir];
-			
-			
-			// 8. 보드의 범위를 벗어나지 않으면서 
-			if(newRow >= 0 && newRow < rowSize && newCol >= 0 && newCol < colSize) {
-				// 이전에 방문하지 않은 노드라면 방문을 한다.
-				if(!visited[ board[newRow][newCol] - 'A' ]) {
-					dfs(newRow, newCol, depth + 1); // 방문
-				}
-			}
-		}
-		
-		// 9. 더이상 방문할 노드가 없다면 최대 이동 거리를 갱신하고 이전 노드로 돌아간다.
-		maxDepth = (depth > maxDepth) ? depth : maxDepth;
-		visited[ board[curRow][curCol] - 'A' ] = false;
-	}
+
 }
