bc880588fe8e69198764b2293cef71462c2780a8
Nooroong <46676443+Nooroong@users.noreply.github.com>
2024-02-23 12:06:29 +0900

    [Gold IV] Title: 치즈, Time: 112 ms, Memory: 13264 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,131 +1,96 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-/**
- * 
- * @author JiYeon Sin
- *
- */
 public class Main {
-	static class Node {
-		int row;
-		int col;
+
+	static BufferedReader br;
+	static StringBuilder sb;
+	static StringTokenizer st;
+	
+	static int boardRowSize, boardColSize;
+	static int[][] board;
+	static int cheese; // 치즈가 다 녹기전 치즈 양을 기록해두는 변수
+	
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	static boolean[][] isVisited;
+	
+	public static void inputTestCase() throws IOException{
+		// 치즈가 놓인 찬의 세로, 가로 길이를 입력받는다.
+		st = new StringTokenizer(br.readLine().trim());
+		boardRowSize = Integer.parseInt(st.nextToken());
+		boardColSize = Integer.parseInt(st.nextToken());
 		
-		Node(int row, int col) {
-			this.row = row;
-			this.col = col;
+		// 판의 상태를 입력받는다.
+		board = new int[boardRowSize][boardColSize];
+		cheese = 0;
+		for (int rowIdx=0; rowIdx<boardRowSize; rowIdx++) {
+			st = new StringTokenizer(br.readLine().trim());
+			for (int colIdx=0; colIdx<boardColSize; colIdx++) {
+				board[rowIdx][colIdx] = Integer.parseInt(st.nextToken());
+				// 치즈가 있는 칸의 수 세기
+				if (board[rowIdx][colIdx]==1)
+					cheese++;
+			}
 		}
 	}
 	
-	public static BufferedReader br;
-	public static StringTokenizer st;
-	public static StringBuilder sb;
+	public static void searchCheese() {
+		Queue<int[]> q = new ArrayDeque<>();
+		q.offer(new int[] {0,0});
+		isVisited[0][0] = true;
+		
+		while(!q.isEmpty()) {
+			int[] current = q.poll();
+			for (int d=0; d<4; d++) {
+				int nextRowIdx = current[0]+dr[d];
+				int nextColIdx = current[1]+dc[d];
+				
+				// 범위 안에 있고 아직 방문하지 않았다면
+				if (nextRowIdx>=0 && nextColIdx>=0 && nextRowIdx<boardRowSize && nextColIdx<boardColSize
+						&& !isVisited[nextRowIdx][nextColIdx]) {
+					isVisited[nextRowIdx][nextColIdx] = true;
+					// 2-1-1. 치즈라면 -> 제거 (0으로 바꿔주기)
+					if (board[nextRowIdx][nextColIdx] == 1) {
+						board[nextRowIdx][nextColIdx] = 0;
+						cheese--;
+					}
+					// 2-1-2. 치즈가 아니라면 -> 큐에 삽입해 근처의 치즈 탐색
+					else {
+						q.offer(new int[] {nextRowIdx, nextColIdx});
+					}
+				}		
+			}
+		}
+	}
 	
-	// 사각형 모양 판의 크기
-	public static int rowSize;
-	public static int colSize;
-	
-	public static int[][] plate; // 사각형 모양의 판
-	
-	public static int leftCheeseCount; // 남은 치즈의 수
-	public static int lastLeftCheeseCount; // 모두 녹기 한 시간 전에 남아있는 치즈의 수
-	public static int hour; // 치즈가 모두 녹는 데 걸리는 시간
-	
-	
-	public static Queue<Node> queue;
-	public static boolean[][] visited;
-	
-	// 상, 우, 하, 좌 이동 방향
-	public static int[] deltaRow = { -1, 0, 1, 0 };
-	public static int[] deltaCol = { 0, 1, 0, -1 };
-	public static final int DIRECTION = 4; // 이동 방향의 수
-	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception{
+
 		br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
 		
-		st = new StringTokenizer(br.readLine().trim());
+		// 1. 테스트케이스를 입력받는다.
+		inputTestCase();
 		
-		rowSize = Integer.parseInt(st.nextToken());
-		colSize = Integer.parseInt(st.nextToken());
-		
-		leftCheeseCount = 0;
-		queue = new ArrayDeque<>();
-		visited = new boolean[rowSize][colSize];
-		
-		plate = new int[rowSize][colSize];
-		for(int rowIdx = 0; rowIdx < rowSize; rowIdx++) {
-			st = new StringTokenizer(br.readLine().trim());
-			
-			for(int colIdx = 0; colIdx < colSize; colIdx++) {
-				plate[rowIdx][colIdx] = Integer.parseInt(st.nextToken());
-				leftCheeseCount += plate[rowIdx][colIdx];
-			}
+		// 2. while: 치즈의 개수가 없어질때까지 무한반복
+		int cheeseSaved = 0;
+		int time = 0;
+		while (cheese!=0) {
+			// 2-1. bfs 실행 전 치즈의 개수 저장
+			cheeseSaved = cheese;
+			// 2-2. (0,0)부터 bfs로 board를 탐색한다
+			isVisited = new boolean[boardRowSize][boardColSize];
+			searchCheese();
+			// 2-3. time 증가
+			time++;
 		}
 		
-		lastLeftCheeseCount = leftCheeseCount;
-		
-		
-		
-		// 배열 전체를 순회하면서 음수인 요소와 맞닿아 있는 치즈를 녹인다.
-//		for(int rowIdx = 0; rowIdx < rowSize; rowIdx++) {
-//			System.out.println(Arrays.toString(plate[rowIdx]));
-//		}
-		
-		hour = 0;
-		while(leftCheeseCount > 0) {
-			visited = new boolean[rowSize][colSize];
-//			System.out.println(leftCheeseCount);
-			lastLeftCheeseCount = leftCheeseCount;
-			
-			queue.offer(new Node(0, 0)); findOutterAirArea();
-			queue.offer(new Node(0, colSize-1)); findOutterAirArea();
-			queue.offer(new Node(rowSize-1, 0)); findOutterAirArea();
-			queue.offer(new Node(rowSize-1, colSize-1)); findOutterAirArea();
-			
-			hour++;
-		}
-		
-		
-		sb.append(hour).append("\n").append(lastLeftCheeseCount);
-		System.out.println(sb);
+		System.out.println(time);
+		System.out.println(cheeseSaved);
 	}
-	
-	
-	public static void findOutterAirArea() {
-		while(!queue.isEmpty()) {
-			Node node = queue.poll();
-			
-			
-			
-			// 치즈를 만나면 탐색 종료
-			if(plate[node.row][node.col] == 1) {
-				plate[node.row][node.col] = 0;
-				leftCheeseCount--;
-				continue;
-			}
-			
-			// 현재 위치가 공기라면 1 감소
-			if(plate[node.row][node.col] == 0) {
-				plate[node.row][node.col]--;
-			}
-			
-			
-			// 다음 위치를 찾는다.
-			for(int dir = 0; dir < DIRECTION; dir++) {
-				int newRow = node.row + deltaRow[dir];
-				int newCol = node.col + deltaCol[dir];
-				
-				if(newRow < 0 || newRow >= rowSize || newCol < 0 || newCol >= colSize) {
-					continue;
-				}
-				
-				if(!visited[newRow][newCol]) {
-					visited[newRow][newCol] = true;
-					queue.offer(new Node(node.row + deltaRow[dir], node.col + deltaCol[dir]));
-				}
-			}
-			
-		}
-	}
+
 }
