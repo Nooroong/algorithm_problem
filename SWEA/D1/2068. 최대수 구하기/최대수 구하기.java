149092e2597373f11361895f607742c9e4d73dda
Nooroong <46676443+Nooroong@users.noreply.github.com>
2024-03-05 15:59:36 +0900

    [D1] Title: 최대수 구하기, Time: 102 ms, Memory: 17,428 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -6,9 +6,8 @@ import java.util.*;
  * @author JiYeon Sin
  * 
  * 1. 테스트 케이스의 개수를 입력받는다.
- * 2. 10개의 숫자를 입력받아 배열에 저장한다.
- * 3. 배열을 오름차순으로 정렬한다.
- * 4. 배열의 가장 마지막 인덱스의 값을 형식에 맞게 출력한다.
+ * 2. 10개의 숫자를 입력받으면서 최대값을 구한다.
+ * 3. 형식에 맞게 출력한다.
  */
 public class Solution {
 	public static BufferedReader br;
@@ -17,7 +16,7 @@ public class Solution {
 	
 	public static int testCase; // 테스트 케이스의 개수
 	
-	public static int[] numberArr; // 숫자들을 담는 배열
+	public static int maxNumber; // 최대값
 	public static final int NUMBER_COUNT = 10; // 숫자들의 개수
 	
 	
@@ -30,18 +29,18 @@ public class Solution {
 		testCase = Integer.parseInt(br.readLine().trim());
 		
 		for(int tc = 1; tc <= testCase; tc++) {
-			numberArr = new int[NUMBER_COUNT];
-			
-			// 2. 10개의 숫자를 입력받아 배열에 저장한다.
+			// 2. 10개의 숫자를 입력받으면서 최대값을 구한다.
+			maxNumber = Integer.MIN_VALUE;
 			st = new StringTokenizer(br.readLine().trim());
-			for(int idx = 0; idx < NUMBER_COUNT; idx++)
-				numberArr[idx] = Integer.parseInt(st.nextToken());
+			for(int idx = 0; idx < NUMBER_COUNT; idx++) {
+				int num = Integer.parseInt(st.nextToken());
+				if(num > maxNumber) maxNumber = num;
+			}
 			
-			Arrays.sort(numberArr); // 3. 배열을 오름차순으로 정렬한다.
 
 			
-			// 4. 배열의 가장 마지막 인덱스의 값을 형식에 맞게 출력한다.
-			sb.append("#").append(tc).append(" ").append(numberArr[NUMBER_COUNT-1]);
+			// 3. 형식에 맞게 출력한다.
+			sb.append("#").append(tc).append(" ").append(maxNumber);
 			System.out.println(sb);
 			sb.setLength(0);
 		}
