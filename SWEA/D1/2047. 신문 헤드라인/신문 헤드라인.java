13ca275e5c633f6d64c107599b5e2a157984d35d
Nooroong <46676443+Nooroong@users.noreply.github.com>
2024-03-08 00:17:47 +0900

    [D1] Title: 신문 헤드라인, Time: 99 ms, Memory: 18,220 KB -BaekjoonHub

--- SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -1,39 +1,44 @@
-/*
- * 1. 문자열을 한 줄 입력 받는다.
- * 2. 입력받은 문자열을 한 문자씩 검사한다.
- * 3-1. 대문자라면 다음 문자로 넘어간다.
- * 3-2. 소문자라면 대문자로 변환한다.(-32)
- * 4. 문자열의 모든 문자에 대한 처리가 끝나면 결과를 출력한다.
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ * @author JiYeon Sin
+ * 
+ * 1. 문자열을 입력받아 char형 배열에 저장한다.
+ * 2. 문자열의 문자를 하나씩 살핀다.
+ * 3. 문자가 소문자에 해당하면 대문자로 변경한다.
+ * 4. 변환된 결과를 출력한다.
  */
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-
 public class Solution {
-	static BufferedReader br;
-
+	public static BufferedReader br;
+	public static StringTokenizer st;
+	public static StringBuilder sb;
+	
+	public static char[] input;
+	
+	
 	public static void main(String[] args) throws IOException {
-		// 소문자가 대문자보다 32만큼 더 크다.
-		// 소문자 a의 아스키코드 값은 97
 		br = new BufferedReader(new InputStreamReader(System.in));
-		String headLine;
-		char oneChar; // 문자열의 문자 하나를 담는다.
+		sb = new StringBuilder();
 		
-		headLine = br.readLine().trim(); // 1. 문자열 입력받기
+		// 1. 문자열을 입력받아 char형 배열에 저장한다.
+		input = br.readLine().trim().toCharArray();
 		
-		// 2. 한 문자씩 검사한다.
-		for(int i = 0; i < headLine.length(); i++) {
-			oneChar = headLine.charAt(i);
-			// 3. 문자가 소문자라면
-			if(oneChar >= 'a' && oneChar <= 'z') {
-				headLine = headLine.replace(oneChar, (char)(oneChar-32));  // 대문자로 변환한다.
+		// 2. 문자열의 문자를 하나씩 살핀다.
+		for(int idx = 0; idx < input.length; idx++) {
+			// 3. 문자가 소문자에 해당하면 대문자로 변경한다.
+			if(input[idx] >= 'a' && input[idx] <= 'z') {
+				// 이렇게 하면 아스키 코드 값을 몰라도 대문자로 변환 가능
+				// 소문자가 대문자보다 값이 크다는 것만 기억하면 된다.
+				input[idx] -= 'a'-'A';
 			}
 		}
 		
-		// 4. 결과를 출력한다.
-		System.out.println(headLine);
-
+		// 4. 변환된 결과를 출력한다.
+		for(int idx = 0; idx < input.length; idx++)
+			sb.append(input[idx]);
+			
+		System.out.println(sb);
 	}
-
 }
