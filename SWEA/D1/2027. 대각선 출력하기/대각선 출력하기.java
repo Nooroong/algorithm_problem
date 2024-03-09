d9ac64a083a0b90ed015157a448ad5f804f623f5
Nooroong <46676443+Nooroong@users.noreply.github.com>
2024-03-09 21:52:02 +0900

    [D1] Title: 대각선 출력하기, Time: 100 ms, Memory: 19,132 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+/**
+ * 
+ * @author JiYeon Sin
+ * 그대로 출력한다.
+ */
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		System.out.println("#++++\n+#+++\n++#++\n+++#+\n++++#");
+	}
+}
