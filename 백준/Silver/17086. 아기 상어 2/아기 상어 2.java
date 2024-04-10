d8ce58829864182fde3e814bce818f1069912982
Nooroong <46676443+Nooroong@users.noreply.github.com>
2024-04-10 14:51:46 +0900

    [Silver II] Title: 아기 상어 2, Time: 96 ms, Memory: 12072 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -5,12 +5,16 @@ import java.util.*;
  * 
  * @author JiYeon Sin
  * 
- * 빈칸에서 8방향으로 bfs를 이용.
- * 빈 칸과 아기 상어까지의 거리는 bfs의 탐색 level이 된다.
+ * 빈 칸을 출발점으로 하여 bfs를 할 수도 있지만
+ * 아기 상어를 출발점으로 하면 좀 더 빠르게 정답을 찾을 수 있다.
+ * 가중치가 없는 그래프에서 bfs는 최단 거리를 찾는다.
  * 
  * 1. 맵의 크기와 정보를 입력받는다.
- * 2. 모든 빈 칸에 대해 bfs를 시도한다.
- * 	2-1. 탐색도중 아기상어를 만나면 최대 거리를 갱신한다.
+ * 	1-1. 아기 상어의 위치를 큐에 저장한다.
+ * 2. 모든 아기 상어에 대해 bfs.
+ * 	2-1. 아직 탐색되지 않은 빈칸이라면 거리를 기록하고 큐에 좌표를 삽입한다.
+ * 		2-1-1. 이때 최대 안전 거리도 갱신한다.
+ * 	2-2. 이미 탐색된 빈칸이라면 이미 다른 아기상어에 의해 최단 거리가 기록된 것이므로 pass
  * 3. 최대 안전 거리를 출력한다.
  *
  */
@@ -27,7 +31,7 @@ public class Main {
 	
 	// bfs
 	static Queue<Point> queue = new ArrayDeque<>();
-	static boolean[][] visited;
+	static int[][] visited;
 	
 	public static void main(String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
@@ -39,34 +43,30 @@ public class Main {
 		colSize = Integer.parseInt(st.nextToken());
 		
 		map = new int[rowSize][colSize];
-		visited = new boolean[rowSize][colSize];
+		visited = new int[rowSize][colSize];
 		
 		// 맵 정보 입력
 		for(int rowIdx = 0; rowIdx < rowSize; rowIdx++) {
 			st = new StringTokenizer(br.readLine().trim());
 			
-			for(int colIdx = 0; colIdx < colSize; colIdx++)
-				map[rowIdx][colIdx] = Integer.parseInt(st.nextToken());
-		}
-		
-		
-		// 2. 모든 빈 칸에 대해 bfs를 시도한다.
-		maxDistance = Integer.MIN_VALUE;
-		
-		for(int rowIdx = 0; rowIdx < rowSize; rowIdx++) { 
 			for(int colIdx = 0; colIdx < colSize; colIdx++) {
-				// 아기 상어가 있는 칸은 패스
-				if(map[rowIdx][colIdx] == 1) continue;
+				map[rowIdx][colIdx] = Integer.parseInt(st.nextToken());
 				
-				// bfs에 사용되는 visited, queue 초기화
-				initVisited();
-				queue.clear();
-				
-				queue.offer(new Point(rowIdx, colIdx));
-				visited[rowIdx][colIdx] = true;
-				bfs();
+				// 1-1. 아기 상어의 위치를 큐에 저장한다.
+				if(map[rowIdx][colIdx] == 1) {
+					queue.offer(new Point(rowIdx, colIdx));
+					visited[rowIdx][colIdx] = -1; // 아기 상어가 있는 곳은 거리를 음수로 표시(미방문 빈칸과 구분)
+				}
 			}
+				
 		}
+		
+		
+		// 2. 모든 아기 상어에 대해 bfs.
+		maxDistance = Integer.MIN_VALUE;
+		bfs();
+
+		
 		
 		// 3. 최대 안전 거리를 출력한다.
 		System.out.println(maxDistance);
@@ -89,24 +89,16 @@ public class Main {
 					int newRow = curRow + deltaRow[dir];
 					int newCol = curCol + deltaCol[dir];
 					
-					// 지도 밖이나 이미 방문한 곳은 pass
-					if(!boundaryCheck(newRow, newCol)) continue;
-					if(visited[newRow][newCol]) continue;
+					// 2-2. 이미 탐색된 빈칸이라면 이미 다른 아기상어에 의해 최단 거리가 기록된 것이므로 pass
+					if(!boundaryCheck(newRow, newCol)) continue; // 얘는 맵을 벗어난 경우
+					if(visited[newRow][newCol] != 0) continue;
 					
-					
+					// 2-1. 아직 탐색되지 않은 빈칸이라면 거리를 기록하고 큐에 좌표를 삽입한다.
 					queue.offer(new Point(newRow, newCol));
-					visited[newRow][newCol] = true;
+					visited[newRow][newCol] = distance;
 					
-					// 2-1. 탐색도중 아기상어를 만나면 최대 거리를 갱신한다.
-					if(map[newRow][newCol] == 1) {
-						maxDistance = Integer.max(distance, maxDistance);
-						
-						// 더이상 탐색할 필요가 없으므로 종료를 위해 큐를 비워버린다.
-						// 안쪽 while문에서 사용하는 size도 0으로 해준다.
-						queue.clear(); 
-						size = 0;
-						break;
-					}
+					// 2-1-1. 이때 최대 안전 거리도 갱신한다.
+					maxDistance = Integer.max(distance, maxDistance);
 
 				}
 			}
@@ -116,11 +108,7 @@ public class Main {
 	}
 	
 	
-	static void initVisited() {
-		for(int rowIdx = 0; rowIdx < rowSize; rowIdx++) 
-			Arrays.fill(visited[rowIdx], false);
-	}
-	
+
 	static boolean boundaryCheck(int rowIdx, int colIdx) {
 		return (rowIdx >= 0 && rowIdx < rowSize && colIdx >= 0 && colIdx < colSize);
 	}
