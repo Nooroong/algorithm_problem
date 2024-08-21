98b16870bb103d7541a84811080e8512558ded0f
Nooroong <46676443+Nooroong@users.noreply.github.com>
2024-08-21 15:59:37 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 68 ms, Memory: 13156 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,57 +1,47 @@
 import java.io.*;
-import java.sql.Array;
 import java.util.*;
 
 public class Main {
     private static BufferedReader br;
     private static StringTokenizer st;
 
-    private static int operandCount,
-                        operandList[],
-                        operationList[],
-                        minResult = Integer.MAX_VALUE, maxResult = Integer.MIN_VALUE,
-                        selectedElementList[];
+    private static int operandCount, operandList[], // 피연산자 수, 피연산자 배열
+                        operationCountList[] = new int[4], // 각 연산자의 수를 저장하는 배열
+                        minResult = Integer.MAX_VALUE, maxResult = Integer.MIN_VALUE;
 
-    private static boolean usedOperationList[];
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
 
+        // 입력받기
         operandCount = Integer.parseInt(br.readLine().trim());
 
+        // 피연산자
         st = new StringTokenizer(br.readLine().trim());
         operandList = new int[operandCount];
         for(int idx = 0; idx < operandCount; idx++) {
             operandList[idx] = Integer.parseInt(st.nextToken());
         }
 
+        // 연산자
         st = new StringTokenizer(br.readLine().trim());
-        operationList = new int[operandCount-1];
-        usedOperationList = new boolean[operandCount-1];
-
-        for(int operationIdx = 0, arrIdx = 0; operationIdx < 4; operationIdx++) {
-
-            int operationCount = Integer.parseInt(st.nextToken());
-
-            for(int count = 0; count < operationCount; count++, arrIdx++)
-                operationList[arrIdx] = operationIdx;
+        for(int idx = 0; idx < 4; idx++) {
+            operationCountList[idx] = Integer.parseInt(st.nextToken());
         }
 
 
-        selectedElementList = new int[operandCount - 1];
-        Permutation(0);
+        // 순열 찾기
+        Permutation(0, operandList[0]);
 
 
+        // 정답 출력
         System.out.println(maxResult + "\n" + minResult);
     }
 
-    private static void Permutation(int selectedElementIdx) {
+
+    private static void Permutation(int selectedElementIdx, int result) {
         // 기저조건
         if(selectedElementIdx == operandCount-1) {
-            
-            // 계산
-            int result = calc();
-
             // 최소, 최대값 갱신
             if(result < minResult) minResult = result;
             if(result > maxResult) maxResult = result;
@@ -59,47 +49,34 @@ public class Main {
             return;
         }
 
-        for(int idx = 0; idx < operationList.length; idx++) {
-            if(usedOperationList[idx])
+
+        for(int idx = 0; idx < operationCountList.length; idx++) {
+            // 현재 연산자를 선택할 수 없는 경우 다음 원소 고르러 가기
+            if(operationCountList[idx] == 0)
                 continue;
 
-            usedOperationList[idx] = true;
 
-            selectedElementList[selectedElementIdx] = operationList[idx];
-            Permutation(selectedElementIdx + 1);
+            // 현재 원소 선택
+            operationCountList[idx]--;
 
-            usedOperationList[idx] = false;
-        }
-    }
-
-    private static int calc() {
-        int result = operandList[0];
-
-        for(int idx = 0; idx < selectedElementList.length; idx++) {
-            switch(selectedElementList[idx]) {
+            // 다음 원소 고르러 가기
+            switch(idx) {
                 case 0:
-                    result += operandList[idx+1];
+                    Permutation(selectedElementIdx + 1, result + operandList[selectedElementIdx+1]);
                     break;
                 case 1:
-                    result -= operandList[idx+1];
+                    Permutation(selectedElementIdx + 1, result - operandList[selectedElementIdx+1]);
                     break;
                 case 2:
-                    result *= operandList[idx+1];
+                    Permutation(selectedElementIdx + 1, result * operandList[selectedElementIdx+1]);
                     break;
                 case 3:
-                    // 음수를 양수로 나누기
-                    if(result < 0) {
-                        result *= -1;
-                        result /= operandList[idx+1];
-                        result *= -1;
-                    } else {
-                        result /= operandList[idx+1];
-                    }
+                    Permutation(selectedElementIdx + 1, result / operandList[selectedElementIdx+1]);
                     break;
             }
+
+            // 선택 복구
+            operationCountList[idx]++;
         }
-
-        return result;
     }
-
 }
